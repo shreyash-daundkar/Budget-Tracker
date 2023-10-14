5f01729040a7c070568f86ed5383103f9ba66bb7
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-14 14:39:53 +0530

    Done Database Setup

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const database = require('./util/database');
+
+
 
 const app = express();
 
@@ -9,7 +12,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
+
 app.use((req, res, next) => res.end('hola amigo'));
 
 
-app.listen(4000);
+
+createServer();
+
+async function createServer() {
+    const res = await database.sync(); 
+    app.listen(4000);
+}
