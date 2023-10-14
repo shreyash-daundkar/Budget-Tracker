c9f06e19a98d21f0caa1cbad57f16a3c4bde2b5c
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-14 15:08:00 +0530

    Done with Signup Froentend Script.

---
+++ Frontend/scripts/signup.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById('signup-form');
+    const alertDiv = document.getElementById('alert-message');
+    
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        
+        const username = document.getElementById('signup-username').value;
+        const email = document.getElementById('signup-email').value;
+        const password = document.getElementById('signup-password').value;
+
+        const userData = {
+            username,
+            email,
+            password,
+            expense: 0,
+        };
+
+        try {
+            const response = await axios.post('YOUR_BACKEND_API_ENDPOINT_HERE', userData);
+            
+            // backend sends { success: true } for successful signups
+            if (response.data.success) {
+                showAlert('Signup successful!', 'success');
+            } else {
+                showAlert('Something went wrong. Please try again.', 'danger');
+            }
+        } catch (error) {
+            // Handling errors like email already exists or other backend validations
+            if (error.response && error.response.data && error.response.data.message) {
+                showAlert(error.response.data.message, 'danger');
+            } else {
+                showAlert('Something went wrong. Please try again.', 'danger');
+            }
+        }
+    });
+
+    function showAlert(message, type) {
+        alertDiv.className = `alert alert-${type}`;
+        alertDiv.textContent = message;
+        alertDiv.style.display = '';
+
+        setTimeout(() => {
+            alertDiv.style.display = 'none';
+        }, 5000);
+    }
+});
