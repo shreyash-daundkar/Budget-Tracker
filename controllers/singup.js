0eeb1c7b0b08ea10fbfea73d71a3776392291fe9
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-14 23:09:41 +0530

    Done with User Controller

---
+++ controllers/singup.js
@@ -0,0 +1,14 @@
+const User = require('../models/users');
+
+exports.addUser = async (req, res, next) => {
+    try{ 
+        const res = await User.create(req.body);
+        res.json({success: true});
+    } catch(error) {
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            res.status(400).send('Email already in use');
+        } else {
+            res.status(500).send('Something went wrong');
+        }
+    }
+}
