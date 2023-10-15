09816d4e9556f2602d520a1f0cd011faeb2df454
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-15 13:58:18 +0530

    Improve code in Login Controller

--- controllers/login.js
+++ controllers/login.js
@@ -3,11 +3,10 @@ const User = require('../models/users');
 exports.verifyUser = async (req, res, next) => {
     try{ 
         const {email, password} = req.body;
-        const users = await User.findAll();
-        const user = users.filter(u => u.email === email);
-        if(user.length === 0) res.status(404).send('Email is not found');
+        const users = await User.findAll({where: {email}});
+        if(users.length === 0) res.status(404).send('Email is not found');
         else {
-            if(user[0].password === password) res.json({success: true});
+            if(users[0].password === password) res.json({success: true});
             else res.status(400).send('Password is incorrect')
         }
     } catch(error) {
