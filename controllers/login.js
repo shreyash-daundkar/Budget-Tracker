af8d1c4380363724d947d52d4e427434a2514fed
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-15 16:45:39 +0530

    Login Api Updated with Bcrypt Compare.

--- controllers/login.js
+++ controllers/login.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/users');
+
 
 exports.verifyUser = async (req, res, next) => {
     try{ 
@@ -6,8 +9,10 @@ exports.verifyUser = async (req, res, next) => {
         const users = await User.findAll({where: {email}});
         if(users.length === 0) res.status(404).send('Email is not found');
         else {
-            if(users[0].password === password) res.json({success: true});
-            else res.status(400).send('Password is incorrect')
+            bcrypt.compare(password, users[0].password, (error, result) => {
+                if(error) res.status(400).send('Password is incorrect');
+                else if(result) res.json({success: true});
+            });
         }
     } catch(error) {
         res.status(500).send('Something went wrong');
