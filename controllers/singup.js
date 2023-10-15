d697b51648c279e96174217c3341157e4e4df3be
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-15 16:25:38 +0530

    Added Password Hashing while Singup

--- controllers/singup.js
+++ controllers/singup.js
@@ -1,8 +1,12 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/users');
 
 exports.addUser = async (req, res, next) => {
     try{ 
-        await User.create(req.body);
+        const user = req.body;
+        user.password = await hashPassword(user.password);
+        await User.create(user);
         res.json({success: true});
     } catch(error) {
         if (error.name === 'SequelizeUniqueConstraintError') {
@@ -11,4 +15,13 @@ exports.addUser = async (req, res, next) => {
             res.status(500).send('Something went wrong');
         }
     }
+}
+
+function hashPassword(password) {
+    return new Promise(resolve => {
+        bcrypt.hash(password, 10, (error, hash) => {
+            if(error) throw error;
+            resolve(hash);
+        });
+    });
 }
