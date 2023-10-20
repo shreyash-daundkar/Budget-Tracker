918024bdb7335a58c15a54765b8fc8a9fae6535e
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-20 10:24:05 +0530

    Added Json Web Token Setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const database = require('./util/database');
 const userRouter = require('./routes/user');
