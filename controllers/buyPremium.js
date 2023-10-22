2a8bc24fb73db95086e289eaa17093eda4e97261
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-22 09:22:06 +0530

    Backend create Order and Send it to Froentend.

--- controllers/buyPremium.js
+++ controllers/buyPremium.js
@@ -1,5 +1,22 @@
-const razorPay = require('razorpay');
+const Razorpay = require('razorpay');
+
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_ir71hDJSQ6jdEK',
+    key_secret: 'BT5qyGPBut8OZJeX6QO45QTK'
+});
 
 exports.getOrderId = (req, res, next) => {
-    console.log(req.user);
+    const option = {
+        amount: '250', 
+        currency: 'INR',
+    };
+    razorpay.orders.create(option, async (error, order) => {
+        if(error) return res.status(500).json({message: 'error creating order'});
+        try {
+            await req.user.createOrder({orderId: order.id, status: 'PENDING'});
+            res.json({order, key_id : razorpay.key_id});
+        } catch (error) {
+            res.status(500).json({message: error.message});
+        }
+    });
 }
