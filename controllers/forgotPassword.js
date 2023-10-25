621f6ae4598f60b93dc60386a62ee85be8df7412
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-25 12:30:21 +0530

    Done with sending  mail

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,4 +1,27 @@
+const Sib = require('sib-api-v3-sdk');
 
-exports.sendMail = (req, res, next) => {
-    console.log(req.body.email);
+
+exports.sendMail = async (req, res, next) => {
+    const { email } = req.body;
+
+    const client = Sib.ApiClient.instance;
+    const apiKey = client.authentication['api-key'];
+    apiKey.apiKey = process.env.SMPTS_KEY;
+
+    const transEmailApi = new Sib.TransactionalEmailsApi();
+
+    const sender = {
+        email: 'shreyashdaundkar@gmail.com',
+        name: 'Budget Tracker',
+    }
+
+    const receivers = [ { email } ];
+
+    const key = await transEmailApi.sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'Forget Password',
+        textContent: 'hey',
+    })
+
 }
