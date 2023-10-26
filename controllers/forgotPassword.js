bb5545f8aea4bc9214aed2b384352187147d2743
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-26 12:43:53 +0530

    Code Improvements in forgot password controller

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,4 +1,5 @@
 const Sib = require('sib-api-v3-sdk');
+const User = require('../models/users');
 
 
 exports.sendMail = async (req, res, next) => {
@@ -18,13 +19,17 @@ exports.sendMail = async (req, res, next) => {
         }
     
         const receivers = [ { email } ];
-    
+        const users = await User.findAll({where: {email}});
+        const { id } = await users[0].createForgotPasswordRequest({ isActive: true});
+
+
         const key = await transEmailApi.sendTransacEmail({
             sender,
             to: receivers,
             subject: 'Forget Password',
-            textContent: 'hey',
+            textContent: `http://127.0.0.1:5501/Frontend/reset-password.html?id=${id}`,
         });  
+
 
         res.json({ key });
 
