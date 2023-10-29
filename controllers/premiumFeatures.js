3c097624ac2dfc3cc50794ee494ac71a972a54a9
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-29 12:22:34 +0530

    Done with S3 upload

--- controllers/premiumFeatures.js
+++ controllers/premiumFeatures.js
@@ -1,3 +1,4 @@
+const Aws = require('aws-sdk');
 const User = require('../models/users');
 
 
@@ -21,6 +22,7 @@ exports.leaderBoard = async (req, res, next) => {
     }
 }    
 
+
 exports.downloadReport = async (req, res, next) => {
     try {
         if(!req.user.isPremium) notPremium(res);
@@ -29,10 +31,43 @@ exports.downloadReport = async (req, res, next) => {
         const fileData = JSON.stringify(expense);
         const fileName = 'Expense' + req.user.id + new Date();
 
-        res.json({fileName, fileData});
+        const location = await storeInS3(fileName, fileData);
+
+        res.json({ location });
 
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({message: 'Error downloding report'});
+    }
+}
+
+
+function storeInS3(fileName, fileData) {
+    try {
+        const s3 = new Aws.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+        });
+       // console.log(s3)
+    
+        const options = {
+            Bucket: 'budget-tracker-57',
+            Key: fileName,
+            Body: fileData,
+            ACL: 'public-read',
+        }
+    
+        return new Promise((resolve, reject) => {
+            s3.upload(options, (error, response) => {
+                if(error) {
+                    console.log(error)
+                    reject(error);
+                }
+                resolve(response.Location);
+            });
+        });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({message: 'Error uploading on s3'});
     }
 }
