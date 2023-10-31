8409012a05a2628b3fca616617ff76df6cfb74fa
Shreyash Daundkar <productiveappa@gmail.com>
2023-10-31 17:03:22 +0530

    Done with Download History Api

--- routes/premium.js
+++ routes/premium.js
@@ -8,5 +8,6 @@ router.get('/buy', buyPremiumController.createOrder);
 router.post('/buy', buyPremiumController.updateOrder);
 router.get('/features/leaderboard', premiumFeatures.leaderBoard);
 router.get('/features/download-report', premiumFeatures.downloadReport);
+router.get('/features/download-history', premiumFeatures.downloadHistory);
 
 module.exports = router;
