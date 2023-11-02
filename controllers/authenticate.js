91150f0403e8ed82be3d7ae9997a0e1d864a48de
Shreyash Daundkar <productiveappa@gmail.com>
2023-11-02 17:43:32 +0530

    Bug fixes and code improvment in authentication controller

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -2,10 +2,16 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
 module.exports = async (req, res, next) => {
-    const token = req.headers['authorization'];
-    const { userId } = decryptData(token);
-    req.user = await User.findByPk(userId);
-    next();
+    try {
+        const token = req.headers['authorization'];
+
+        const { userId } = decryptData(token);
+        req.user = await User.findByPk(userId);
+
+        next();
+    } catch (error) {
+        return res.status(404).json({message: 'invalid token'});
+    }
 }
 
 function decryptData(token) {
