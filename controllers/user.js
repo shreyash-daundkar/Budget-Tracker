5527687e34ad5aeeb62d8bd26645b6d72bcc0d2b
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-08 19:14:16 +0530

    Merged sign up and login into user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,36 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/usersModel');
+const User = require('../models/user');
+
+
+
+exports.addUser = async (req, res, next) => {
+    try{ 
+        const user = req.body;
+        user.password = await hashPassword(user.password);
+        
+        await User.create({...user, isPremium: false, expense: 0});
+        res.json({success: true});
+    } catch(error) {
+        console.log(error);
+
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            res.status(400).send('Email already in use');
+        } else {
+            res.status(500).send('Something went wrong');
+        }
+    }
+}
+
+function hashPassword(password) {
+    return new Promise(resolve => {
+        bcrypt.hash(password, 10, (error, hash) => {
+            if(error) throw error;
+            resolve(hash);
+        });
+    });
+}
 
 
 exports.verifyUser = async (req, res, next) => {
