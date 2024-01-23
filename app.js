3934a7942a266bde219814ab713735e9eec27893
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-23 15:53:54 +0530

    Added mongodb and mongoose setup

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
-const database = require('./util/database');
+//const database = require('./util/database');
+const mongoose = require('mongoose');
 
 const userRouter = require('./routes/user');
 const expenseRouter = require('./routes/expense');
@@ -28,13 +29,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(['/expense','/premium-features','/premium-buy','/download-history'], authenticate);
-app.use('/user', userRouter);
-app.use('/expense', expenseRouter);
-app.use('/premium-features', premiumFeatureRoute);
-app.use('/premium-buy', buyPremiumRoute);
-app.use('/forgot-password', forgotPasswordRoute);
-app.use('/download-history', downloadHistoryRoute);
+// app.use(['/expense','/premium-features','/premium-buy','/download-history'], authenticate);
+// app.use('/user', userRouter);
+// app.use('/expense', expenseRouter);
+// app.use('/premium-features', premiumFeatureRoute);
+// app.use('/premium-buy', buyPremiumRoute);
+// app.use('/forgot-password', forgotPasswordRoute);
+// app.use('/download-history', downloadHistoryRoute);
 
 app.use('/', (req, res, next) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
@@ -44,20 +45,23 @@ app.use('/', (req, res, next) => {
 
 
 
-User.hasMany(Expense);
-Expense.belongsTo(User);
+// User.hasMany(Expense);
+// Expense.belongsTo(User);
 
-User.hasMany(Order);
-Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsTo(User);
 
-User.hasMany(ForgotPasswordRequests);
-ForgotPasswordRequests.belongsTo(User);
+// User.hasMany(ForgotPasswordRequests);
+// ForgotPasswordRequests.belongsTo(User);
 
-User.hasMany(DownloadHistory);
-DownloadHistory.belongsTo(User);
+// User.hasMany(DownloadHistory);
+// DownloadHistory.belongsTo(User);
 
 
 
-database.sync()
- .then(() => app.listen(process.env.PORT || 4000))
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_NAME}.kaqa2nx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+ .then(() => {
+    console.log('db connected');
+    app.listen(process.env.PORT || 4000)
+ })
  .catch(error => console.log(error));
